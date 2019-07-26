5d44273d9d7e8a76e2f58fbe79ba3aa5952f74f7
Frederico José <fredpelogia@outlook.com>
2019-07-26 18:11:04 -0300

    Base para o genoma adicionada

--- Assets/Scripts/InGame/NewbornGenerator.cs
+++ Assets/Scripts/InGame/NewbornGenerator.cs
@@ -8,6 +8,8 @@ public class NewbornGenerator : MonoBehaviour
     public GameObject entityPrefab;
     public float MapRadius = 18;
 
+    public float tolerance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,6 +47,27 @@ public class NewbornGenerator : MonoBehaviour
 
                     int newborn_food_qtty = Mathf.FloorToInt(0.5f*(hitColliders[i].gameObject.GetComponent<Ett_Move>().food_qtty + hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().food_qtty));
                     newbornObj.GetComponent<Ett_Move>().food_qtty = newborn_food_qtty;
+
+                    float newborn_foodRange = 0.5f*(hitColliders[i].gameObject.GetComponent<Ett_Move>().foodRange + hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().foodRange);
+                    newborn_foodRange += Random.Range(-1.0f*tolerance*newborn_foodRange,tolerance*newborn_foodRange);
+                    newbornObj.GetComponent<Ett_Move>().foodRange = newborn_foodRange;
+
+                    float newborn_partnerRange = 0.5f*(hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerRange + hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().partnerRange);
+                    newborn_partnerRange += Random.Range(-1.0f*tolerance*newborn_partnerRange,tolerance*newborn_partnerRange);
+                    newbornObj.GetComponent<Ett_Move>().partnerRange = newborn_partnerRange;
+
+                    int newborn_partnerHunger = Mathf.FloorToInt(0.5f*(hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerHunger + hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().partnerHunger));
+                    newborn_partnerHunger += Mathf.FloorToInt(Random.Range(-1*tolerance*newborn_partnerHunger,tolerance*newborn_partnerHunger));
+                    newbornObj.GetComponent<Ett_Move>().partnerHunger = newborn_partnerHunger;
+
+                    float newborn_wanderRadius = 0.5f*(hitColliders[i].gameObject.GetComponent<Ett_Move>().wanderRadius + hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().wanderRadius);
+                    newborn_wanderRadius += Random.Range(-1.0f*tolerance*newborn_wanderRadius,tolerance*newborn_wanderRadius);
+                    newbornObj.GetComponent<Ett_Move>().wanderRadius = newborn_wanderRadius;
+
+                    float newborn_wanderTimer = 0.5f*(hitColliders[i].gameObject.GetComponent<Ett_Move>().wanderTimer + hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().wanderTimer);
+                    newborn_wanderTimer += Random.Range(-1.0f*tolerance*newborn_wanderTimer,tolerance*newborn_wanderTimer);
+                    newbornObj.GetComponent<Ett_Move>().wanderTimer = newborn_wanderTimer;
+
                     
 
                     hitColliders[i].gameObject.GetComponent<Ett_Move>().partnerObj.GetComponent<Ett_Move>().isLookingForPartner = false;
