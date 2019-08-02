0b3245f032092f032deb93b6c7a45eb9b502ff57
fvcalderan <fvcalderan@gmail.com>
2019-08-02 20:17:57 -0300

    Menu adicionado e Reorganização
    Além disso, agora a tecla Esc, quando em jogo, retorna ao menu.

---
+++ Assets/Scripts/Menu/MM_Controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MM_Controls : MonoBehaviour
+{
+
+    public Button BTN_Start;
+    public Button BTN_Quit;
+
+    public Dropdown drop_Map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BTN_Start.onClick.AddListener(BTN_Start_on_Click);
+        BTN_Quit.onClick.AddListener(BTN_Quit_on_Click);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BTN_Start_on_Click()
+    {
+        SceneManager.LoadScene(drop_Map.options[drop_Map.value].text);
+    }
+
+    void BTN_Quit_on_Click()
+    {
+        //Ignored while inside Editor
+        Application.Quit();
+    }
+
+
+}
